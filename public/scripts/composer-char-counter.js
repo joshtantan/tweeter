86e50148726390e767f8431230cc5fa1e190e563
Joshua Tan <josh.tan.tann@gmail.com>
2020-10-07 23:48:02 -0700

    Add tweet char counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
-  // --- our code goes here ---
+  console.log('DOC loaded & ready');
+});
+
+$('#tweet-text').on('input', function() {
+  const charsRemaining = 140 - $(this).val().length;
+  const charCounter = $(this).parent().children('div').children('output.counter');
+  charCounter.html(charsRemaining);
+
+  if (charsRemaining < 0) {
+    charCounter.css('color', '#FF0000');
+  } else {
+    charCounter.css('color', '#545149');
+  }
 });

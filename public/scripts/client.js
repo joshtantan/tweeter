7336bbc2f5348ad4451943695ac86a380e8b3959
Joshua Tan <josh.tan.tann@gmail.com>
2020-10-08 19:17:25 -0700

    Add ajax load tweets from db

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,27 +1,9 @@
-const tweetData = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
+const loadTweets = function() {
+  $.ajax('/tweets', { method: 'GET' })
+  .then(function (jsonRes) {
+    renderTweets(jsonRes);
+  });
+}
 
 const renderTweets = function(tweets) {
   for (const tweet of tweets) {
@@ -69,4 +51,4 @@ $("form").submit(function(event) {
   event.preventDefault();
 });
 
-renderTweets(tweetData);
+loadTweets();
